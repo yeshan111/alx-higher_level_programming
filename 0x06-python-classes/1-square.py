46d6877cf2752f5a14352e366e72af8e7bf66c2d
yeshan111 <yeshan111@gmail.com>
2022-09-13 11:06:41 -0700

    1 fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""a square class"""
+# 2-square.py
+"""Define a class Square."""
 
 
 class Square:
-    """Derives a square """
-    def __init__(self, size):
-        """Initializes attributes
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
         Args:
-            size (int): value to initialize `size`
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
+            size (int): The size of the new square.
         """
-        self.__size = size1
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
